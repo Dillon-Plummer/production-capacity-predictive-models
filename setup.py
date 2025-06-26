a36a78fa9a8fe55b8c362866ba220734c0d365ba
Dillon Plummer <40150199+DillonPlummer@users.noreply.github.com>
2025-06-25 22:34:01 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qualitylab",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "scikit-learn",
+        "streamlit",
+        "seaborn",
+        "matplotlib",
+        "joblib",
+        "openpyxl",
+        "xlrd"
+    ],
+    entry_points={
+        "console_scripts": [
+            "qualitylab=qualitylab.cli:cli",
+        ]
+    }
+)
